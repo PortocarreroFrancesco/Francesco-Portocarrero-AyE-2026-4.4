21a24c3dfcdec92e1a441e7cb5581963c0137228
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-16 08:42:43 -0300

    Implement loop to print 'Hola mundo'
    Added a loop to print 'Hola mundo' five times.

--- Ejercicio 2.cs
+++ Ejercicio 2.cs
@@ -1,6 +1,4 @@
-﻿Build started at 16:11...
-1>------ Build started: Project: ConsoleApp4, Configuration: Debug Any CPU ------
-1>  Skipping analyzers to speed up the build. You can execute 'Build' or 'Rebuild' command to run analyzers.
-1>  ConsoleApp4 -> C:\Users\zaraa\source\repos\ConsoleApp4\ConsoleApp4\bin\Debug\net10.0\ConsoleApp4.dll
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 16:12 and took 12,117 seconds ==========
+for (int i = 0; i<5; i++)
+{
+    Console.WriteLine("Hola mundo");
+}
