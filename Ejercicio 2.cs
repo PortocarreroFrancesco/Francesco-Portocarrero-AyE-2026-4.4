76609b10c063e72a80d4120daab0120308aa0eab
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-21 12:12:51 -0300

    Add files via upload

---
+++ Ejercicio 2.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingresa una frase para invertirla");
+string texto = Console.ReadLine()??"";
+
+string invertido = "";
+
+for(int i=texto.Length-1;i>=0;i--)
+{
+    invertido = invertido + texto[i];
+}
+
+Console.WriteLine(invertido);
