f01c75ea1417ee60dc9a470f0750957a2678f63c
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-21 12:59:04 -0300

    Add files via upload

---
+++ Ejercicio 3.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Ingresá un número y vamos a sumar cada uno de sus digitos");
+string texto = Console.ReadLine()??"";
+
+int AUX = 0;
+
+foreach (char t in texto)
+{
+    AUX = AUX + (t - '0');
+}
+
+Console.WriteLine(AUX);
+
