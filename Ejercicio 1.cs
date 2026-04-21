d579156780f9b3523f456a313a7ce4ba95d6fffa
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-21 11:57:43 -0300

    Add files via upload

---
+++ Ejercicio 1.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese una frase");
+string texto = Console.ReadLine()??"";
+
+int contador = 0;
+string vocales = "aeiouAEIOU";
+
+foreach(char t in texto)
+{
+    foreach (char v in vocales)
+    {
+        if (t == v)
+        {
+            contador++;
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"La frase tiene {contador} vocales");
