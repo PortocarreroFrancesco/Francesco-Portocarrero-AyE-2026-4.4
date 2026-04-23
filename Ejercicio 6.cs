ccb03d1714644f600bb0a92d91bc4eb0901558f4
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-23 10:33:19 -0300

    Add files via upload

---
+++ Ejercicio 6.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Ingresá tu nombre");
+string nombre = Console.ReadLine()??"";
+
+static Boolean palindromo(string nombre)
+{
+    for (int i = 0; i<nombre.Length/2;i++)
+    {
+        if (nombre[i] == nombre[nombre.Length - 1 - i])
+        {
+
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+    return true;
+}   
+
+Boolean resultado = palindromo(nombre);
+if (resultado == true)
+{
+    Console.WriteLine("Es palíndromo");
+}
+else
+{
+    Console.WriteLine("No es palíndromo");
+}
