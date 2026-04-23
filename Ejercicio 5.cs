e026530a4d6eacd2c443712c2766d762de6d5b0f
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-23 09:21:29 -0300

    Add files via upload

---
+++ Ejercicio 5.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingresá tu nombre");
+string nombre = Console.ReadLine()??"";
+Console.WriteLine("Ingresá tu apellido");
+string apellido = Console.ReadLine() ?? "";
+
+char caracter1 = nombre[0];
+char caracter2 = apellido[0];
+
+Console.WriteLine($"{caracter1}.{caracter2}");
+Console.WriteLine(nombre);
+Console.WriteLine(apellido);
+
+
