1efc57ada4928b02afa2508441cebdaaacdf2b81
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-24 13:37:53 -0300

    Update Ejercicio 5.cs

--- Ejercicio 5.cs
+++ Ejercicio 5.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Ingresá tu edad:");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("¿Compraste palomitas de maíz?");
+Console.WriteLine("1) Si");
+Console.WriteLine("2) No");
+
+int palomitas = Convert.ToInt32(Console.ReadLine());
+
+if (edad > 65 && palomitas == 1)
+{
+    Console.WriteLine("¡Felicidades! Tienes entrada gratuita al cine.");
+}
+else
+{
+    Console.WriteLine("Compra la entrada o raja de acá");
+}
