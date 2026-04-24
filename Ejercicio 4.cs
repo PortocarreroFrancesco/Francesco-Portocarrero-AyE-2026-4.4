43eca702d4859c51dae97aa33c6ca9affd09ca44
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-24 13:38:46 -0300

    Implement sum calculation in Ejercicio 4

--- Ejercicio 4.cs
+++ Ejercicio 4.cs
@@ -1,1 +1,17 @@
-﻿
+﻿int suma = 0;
+int numero;
+
+while (true)
+{
+    Console.WriteLine("Ingresá un número (0 para terminar):");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero == 0)
+    {
+        break;
+    }
+
+    suma = suma + numero;
+}
+
+Console.WriteLine("La suma total es: " + suma);
