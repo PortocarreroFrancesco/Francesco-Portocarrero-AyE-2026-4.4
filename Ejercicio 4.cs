61291ba915d8bf014cc89528377462d32b485c6d
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-24 13:37:16 -0300

    Update Ejercicio 4.cs

--- Ejercicio 4.cs
+++ Ejercicio 4.cs
@@ -1,1 +1,11 @@
-﻿
+Console.WriteLine("Ingresá un número entero:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par.");
+}
+else
+{
+    Console.WriteLine("El numero es impar");
+}﻿
