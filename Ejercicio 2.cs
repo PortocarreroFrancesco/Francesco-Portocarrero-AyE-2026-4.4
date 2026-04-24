b19ed5c80d076ec7a0f5f9496399652ba89e048e
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-04-24 15:06:43 -0300

    Update Ejercicio 2.cs

--- Ejercicio 2.cs
+++ Ejercicio 2.cs
@@ -1,10 +1,12 @@
-﻿for (int i = 5; i >= 1; i--)
+int resp;
+
+Console.WriteLine("Listos para despegar? 1) si 2) no");
+resp = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 5; i >= 1; i--)
 {
     Console.WriteLine(i);
 }
-int resp = 0;
-Console.WriteLine("Listos para despegar? 1) si 2)no");
-resp = Convert.ToInt32(Console.ReadLine());
 
 if (resp == 1)
 {
