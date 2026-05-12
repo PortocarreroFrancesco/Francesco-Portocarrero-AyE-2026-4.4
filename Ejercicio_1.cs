65e80cda7dd7a81c6051b14c4d4c0a1028c7f1e9
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-12 13:04:31 -0300

    Add files via upload

---
+++ Ejercicio_1.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Para calcular su salario semanal, ingrese la cantidad de horas que trabajó esta semana.");
+string horasxtrabajo = Console.ReadLine()??"";
+static int salario(int horas)
+{
+    if (horas < 40)
+    {
+        int sueldo = horas * 16;
+        return sueldo;
+    }
+    else
+    {
+        int horasExtra = horas - 40;
+        int sueldo = (horas - horasExtra) * 16 + horasExtra * 20;
+        return sueldo;
+    }
+}
+
+if (int.TryParse(horasxtrabajo, out int horas))
+{
+    Console.WriteLine($"Su salario semanal es de: ${salario(horas)}");
+}
+else
+{
+    Console.WriteLine("Caracter inválido");
+}
---
+++ Ejercicio_2.cs
@@ -0,0 +1,26 @@
+﻿static string suma()
+{
+    Console.WriteLine("Ingresá números para sumarlos, cuando quieras terminar escribí 0");
+    string numeros = Console.ReadLine() ?? "";
+    int acum = 0;
+
+    if (int.TryParse(numeros, out int numero))
+    {
+        acum = acum + numero;
+        if (numero == 0)
+        {
+            string respuesta = $"Resultado: {acum}";
+            return respuesta;
+        }
+        suma();
+    }
+    else
+    {
+        string respuesta = "Carácter no válido.";
+        return respuesta;
+    }
+    string respuesta1 = "error";
+    return respuesta1;
+}
+
+Console.WriteLine(suma());
