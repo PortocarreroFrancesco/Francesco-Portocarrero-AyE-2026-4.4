8c347766c3cb746465f5c2a6218b0a940e2ae1d0
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-21 10:43:46 -0300

    Add files via upload

---
+++ Ejercicio_1.cs
@@ -0,0 +1,12 @@
+﻿string[] Nombres = { "Alejandro", "Alexander", "Augusto", "Baldomero", "Baraquiel", "Bartolomé", "Benedicto", "Adriano", "Gianluca", "Apolo" };
+string NombreMasLargo = Nombres[0]; 
+
+for (int i = 0; i<Nombres.Length;i++)
+{
+    if (Nombres[i].Length > NombreMasLargo.Length)
+    {
+        NombreMasLargo = Nombres[i];
+    }
+}
+
+Console.WriteLine($"El nombre más largo es: {NombreMasLargo}.");
