4c365349a00b59f429aec27d78dd001c606cd4b9
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-21 11:07:27 -0300

    Add files via upload

---
+++ Ejercicio_2.cs
@@ -0,0 +1,14 @@
+﻿string[] Nombres = { "Alejandro", "Alexander", "Augusto", "Baldomero", "Baraquiel", "Bartolomé", "Benedicto", "Adriano", "Gianluca", "Apolo" };
+char[] Vocales = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U', 'á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú' };
+int CantidadVocales = 0;
+for (int i = 0; i<Nombres.Length;i++)
+{
+    foreach (char x in Nombres[i])
+    {
+        foreach (char y in Vocales)
+        {
+            if (x == y) CantidadVocales++;
+        }
+    }
+}
+Console.WriteLine($"Cantidad de vocales: {CantidadVocales}.");
