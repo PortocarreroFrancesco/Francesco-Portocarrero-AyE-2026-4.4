577b11309a809b84621f7343680f62c900b1887e
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-21 11:31:53 -0300

    Add files via upload

---
+++ Ejercicio_3.cs
@@ -0,0 +1,11 @@
+﻿int[] Numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int Factor = 67;
+int[] Tabla;
+Tabla = new int[Numeros.Length];
+for (int i = 0; i<Numeros.Length; i++)
+{
+    int Numero = Numeros[i] * Factor;
+    Tabla[i] = Numero;
+}
+Console.WriteLine("Nueva lista:");
+for (int i = 0; i < Tabla.Length; i++) Console.WriteLine(Tabla[i]);
