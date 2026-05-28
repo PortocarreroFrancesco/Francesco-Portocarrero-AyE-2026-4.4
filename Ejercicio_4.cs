1ddfe57c64340708e4e1214469af1de8d0d5db20
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-28 11:11:55 -0300

    Add files via upload

---
+++ Ejercicio_4.cs
@@ -0,0 +1,23 @@
+﻿float[] Temperatura = new float[7];
+int IndiceTemperatura = 0;
+for (int i = 0; i < Temperatura.Length; i++){
+    Console.WriteLine($"Ingresá la temperatura en grados celsius del dia {i + 1}");
+    string T = Console.ReadLine() ?? "";
+    if (float.TryParse(T, out float Temp)){
+        Temperatura[IndiceTemperatura] = Temp;
+        IndiceTemperatura++;
+    }else{
+        Console.WriteLine("Caracter no válido.");
+        break;
+     }
+} float TemperaturaMinima = Temperatura[0]; 
+for(int i = 0; i < Temperatura.Length; i++){
+    if (TemperaturaMinima > Temperatura[i]){
+        TemperaturaMinima = Temperatura[i];
+    }
+} float TemperaturaMaxima = Temperatura[0];
+for (int i = 0; i < Temperatura.Length; i++){
+    if (TemperaturaMaxima < Temperatura[i]){
+        TemperaturaMaxima = Temperatura[i];
+    }
+} Console.WriteLine($"Temperatura mínima: {TemperaturaMinima}, temperatura máxima: {TemperaturaMaxima}.");
