45ac2325c5450837a56c97f7f0000541b5c5387e
PortocarreroFrancesco <francescopaguilar@gmail.com>
2026-05-29 15:04:11 -0300

    Add files via upload

---
+++ Ejercicio_3(sin_completar).cs
@@ -0,0 +1,20 @@
+﻿float SueldoNeto;
+float Remuneraciones;
+float SueldoBasico;
+int Antiguedad;
+int Presentismo;
+
+float Descuentos;
+float Jubilacion;
+float ObraSocial;
+
+static float CalcularAntiguedad()
+{
+
+}
+Console.WriteLine("Ingrese su sueldo básico.");
+SueldoBasico = Console.ReadLine() ?? "";
+
+Descuentos = Jubilacion + ObraSocial;
+Remuneraciones = SueldoBasico + Antiguedad + Presentismo;
+SueldoNeto = Remuneraciones - Descuentos;
